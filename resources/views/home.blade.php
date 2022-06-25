0c7349b86c609e3f38f570491ee0c775f5fd8907
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2022-06-25 16:21:41 +0700

    slit layout blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,13 +33,13 @@
                 <div class="item-slick1" style="background-image: url({{ url('site') }}/images/slide-2.jpg);">
                     <div class="container h-full">
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-                            <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
+                            <div class="layer-slick1 animated visible-false" data-appear="slideInRight" data-delay="0">
                                 <span class="ltext-101 cl2 respon2">
                                     PHONG CÁCH THỜI TRANG NAM MỚI
                                 </span>
                             </div>
 
-                            <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
+                            <div class="layer-slick1 animated visible-false" data-appear="slideInLeft" data-delay="800">
                                 <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
                                     Jackets & Coats
                                 </h2>
@@ -58,19 +58,19 @@
                 <div class="item-slick1" style="background-image: url({{ url('site') }}/images/slide-3.jpg);">
                     <div class="container h-full">
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-                            <div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
+                            <div class="layer-slick1 animated visible-false" data-appear="zoomInRight" data-delay="0">
                                 <span class="ltext-101 cl2 respon2">
                                     Women Collection 2022
                                 </span>
                             </div>
 
-                            <div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
+                            <div class="layer-slick1 animated visible-false" data-appear="slideInRight" data-delay="800">
                                 <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
                                     New arrivals
                                 </h2>
                             </div>
 
-                            <div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
+                            <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
                                 <a href="#"
                                     class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                                     Đặt ngay
@@ -241,7 +241,7 @@
                                 <div class="block2-pic hov-img0">
                                     <img src="{{ url('uploads') }}/product/{{ $item->image }}" alt="IMG-PRODUCT">
 
-                                    <a href="#"
+                                    <a href="{{ route('home.view', ['id' => $item->id, 'slug' => $item->slug]) }}"
                                         class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 ">
                                         THÔNG TIN
                                     </a>
@@ -284,17 +284,4 @@
     </section>
 
 @endsection
-@section('js')
-<script>
-    // get url selected
-    $(document).ready(function (){
-      $('#select_price').on('change', function () {
-          var url = $(this).val();
-          if (url) {
-              window.location = url; // redirect
-          }
-          return false;
-      });
-    })
-</script>
-@endsection
+

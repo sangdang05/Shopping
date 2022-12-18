e9c7cb3a79fd577c2347337435a1a5e3fb9946f0
programmingforlife2605 <92969533+songvedem97@users.noreply.github.com>
2022-12-19 00:59:35 +0700

    update project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,8 +7,8 @@ use App\Models\Category;
 use Illuminate\Support\Facades\View;
 use App\Helpers\CartHelper;
 use Illuminate\Pagination\Paginator;
-
-
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,8 +30,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        $category=Category::where('status',1)->orderBy('created_at','DESC')->limit(3)->get();
-        view()->share(compact('category'));
-
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS', 'on');
+            URL::forceScheme('https');
+        }
+        if (Schema::hasTable('category')) {
+            $category = Category::where('status', 1)->orderBy('created_at', 'DESC')->limit(3)->get();
+            view()->share(compact('category'));
+        }
     }
 }
